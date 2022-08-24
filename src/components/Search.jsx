976e28b7261eb9443566db5ba6260a735ccc6e1f
Roman Chuchev <romanok20@gmail.com>
2022-08-24 17:42:46 +0300

    Add Search

---
+++ src/components/Search.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+function Search({ cb = Function.prototype }) {
+  const [value, setValue] = useState("");
+
+  const handleKey = (e) => {
+    if (e.key === "Enter") {
+      handleSubmit();
+    }
+  };
+
+  const handleSubmit = () => {
+    cb(value);
+  };
+
+  return (
+    <div className="row">
+      .
+      <div className="form-group display-flex">
+        <input
+          className="form-control "
+          type="search"
+          id="search-field"
+          placeholder="Search"
+          onKeyDown={handleKey}
+          onChange={(e) => setValue(e.target.value)}
+          value={value}
+        />
+        <button className="btn btn-outline-light " onClick={handleSubmit}>
+          Search
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export { Search };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,48 @@
 import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import { getAllCategories } from "../api";
 import Spinner from "../components/spinner";
 import { CategoryList } from "../components/CategoryList";
+import { Search } from "../components/Search";
 
 function Home() {
   const [catalog, setCatalog] = useState([]);
+  const [filteredCatalog, setFilteredCatalog] = useState([]);
+
+  const { pathname, search } = useLocation();
+  const push = useNavigate();
+
+  const handleSearch = (str) => {
+    setFilteredCatalog(
+      catalog.filter((item) =>
+        item.strCategory.toLowerCase().includes(str.toLowerCase())
+      )
+    );
+    push({ pathname, search: `?search=${str}` });
+  };
 
   useEffect(() => {
     getAllCategories().then((data) => {
       setCatalog(data.categories);
+      setFilteredCatalog(
+        search
+          ? data.categories.filter((item) =>
+              item.strCategory.toLowerCase().includes(search.split("=")[1])
+            )
+          : data.categories
+      );
     });
-  }, []);
+  }, [search]);
 
   return (
-    <>{!catalog.length ? <Spinner /> : <CategoryList catalog={catalog} />}</>
+    <>
+      <Search cb={handleSearch} />
+      {!catalog.length ? (
+        <Spinner />
+      ) : (
+        <CategoryList catalog={filteredCatalog} />
+      )}
+    </>
   );
 }
 
